7e1b0fea76e8e81d5e08ccd603d6bbcbdad9931c
Brandon Kase <brandon.kase@gmail.com>
2017-12-31 16:54:35 -0500

    Include product in swift package (#9)

--- Package.swift
+++ Package.swift
@@ -4,6 +4,11 @@ import PackageDescription
 
 let package = Package(
     name: "DoctorPretty",
+    products: [
+        .library(
+            name: "DoctorPretty",
+            targets: ["DoctorPretty"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/typelift/Algebra.git", .exact("0.2.0")),
         .package(url: "https://github.com/typelift/Swiftx.git", .exact("0.6.0")),
